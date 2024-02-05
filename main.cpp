e2b5bf415dfd6baaa11ab0bd8c9886633bf401b6
Pedro Gabriel Amorim Soares <pedrogabrielbhz@gmail.com>
2024-02-04 21:33:50 -0300

    Refactor triangle creation code

--- main.cpp
+++ main.cpp
@@ -17,22 +17,26 @@ GLuint VAO, VBO, shader;
  */
 void CreateTriangle()
 {
-    // Create a vertex array object
+    // Define the vertices of the triangle
     GLfloat vertices[] = {
         -1.0f, -1.0f, 0.0f,
         1.0f, -1.0f, 0.0f,
         0.0f, 1.0f, 0.0f};
 
+    // Create a vertex array object
     glGenVertexArrays(1, &VAO);
     glBindVertexArray(VAO);
 
+    // Create a vertex buffer object
     glGenBuffers(1, &VBO);
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
+    // Set the vertex attribute pointers
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
     glEnableVertexAttribArray(0);
 
+    // Unbind the VBO and VAO
     glBindBuffer(GL_ARRAY_BUFFER, 0);
     glBindVertexArray(0);
 }
